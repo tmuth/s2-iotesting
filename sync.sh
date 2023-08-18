4224b05025b00bfaf3eb822310f76f0dd3e5b1d8
EC2 Default User <ec2-user@ip-172-31-15-178.ec2.internal>
2023-08-18 14:04:36 +0000

    Added lookups

--- sync.sh
+++ sync.sh
@@ -5,5 +5,6 @@ DEST_BASE=/root/s2-iotesting
 
 rsync -avzh /opt/splunk/etc/system/local /root/s2-iotesting/splunk/system
 rsync -avzh /opt/splunk/etc/apps/s2_io_testing /root/s2-iotesting/splunk/apps
+rsync -avzh /opt/splunk/etc/apps/search/lookups/test*.csv /root/s2-iotesting/splunk/lookups/
 
 chown -R ec2-user:ec2-user $DEST_BASE/*
