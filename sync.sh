a2e37aa962c30913eb6925e09c95791f51d6d875
tmuth <tmuth@splunk.com>
2023-08-27 14:48:47 +0000

    fixed root dir

--- sync.sh
+++ sync.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-DEST_BASE=/root/s2-iotesting
+DEST_BASE=~/s2-iotesting
 
 if [ "$HOSTNAME" = i3en6xl ]; then
     printf '%s\n' "Running on $HOSTNAME"
-    sudo rsync -avzh /opt/splunk/etc/system/local /root/s2-iotesting/splunk/system
-    sudo rsync -avzh /opt/splunk/etc/apps/search/lookups/aws*.csv /root/s2-iotesting/splunk/lookups/
+    sudo rsync -avzh /opt/splunk/etc/system/local ~/s2-iotesting/splunk/system
+    sudo rsync -avzh /opt/splunk/etc/apps/search/lookups/aws*.csv ~/s2-iotesting/splunk/lookups/
     sudo chown -R ec2-user:ec2-user $DEST_BASE/*
 fi
 
 if [ "$HOSTNAME" = mini-splunk ]; then
     printf '%s\n' "Running on $HOSTNAME"
-    sudo rsync -avzh /root/s2-iotesting/splunk/lookups/*.csv /opt/splunk/etc/apps/s2_io_testing/lookups/
-    sudo rsync -avzh /opt/splunk/etc/apps/s2_io_testing /root/s2-iotesting/splunk/apps
+    sudo rsync -avzh ~/s2-iotesting/splunk/lookups/*.csv /opt/splunk/etc/apps/s2_io_testing/lookups/
+    sudo rsync -avzh /opt/splunk/etc/apps/s2_io_testing ~/s2-iotesting/splunk/apps
     sudo chown -R ec2-user:ec2-user $DEST_BASE/*
 fi
 
 
 if [ "$HOSTNAME" = azure-s2-test1 ]; then
     printf '%s\n' "Running on $HOSTNAME"
-    sudo rsync -avzh /opt/splunk/etc/apps/search/lookups/azure*.csv /root/s2-iotesting/splunk/lookups/
+    sudo rsync -avzh /opt/splunk/etc/apps/search/lookups/azure*.csv ~/s2-iotesting/splunk/lookups/
     sudo chown -R azureuser:azureuser $DEST_BASE/*
 fi
 
