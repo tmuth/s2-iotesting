afa4e55212c8f1b7af939b42d09faaa1ff89022b
tmuth <tmuth@splunk.com>
2023-09-22 13:42:26 +0000

    Created for new tests

--- run-jmeter-tests.sh
+++ run-jmeter-tests.sh
@@ -3,8 +3,8 @@
 INDEX_NAME=test_high_cardinality_compressed1
 HOME_PATH_PREFIX=/mnt/nvme/splunk
 EARLIEST="01/01/2023:06:00:00"
-LATEST="01/31/2023:22:00:00"
-NAME_PREFIX="aws_31day_b_"
+LATEST="01/02/2023:22:00:00"
+NAME_PREFIX="test_a"
 
 IO_READ_LIMIT_BYTES=""
 IO_READ_LIMIT_MB=""
@@ -43,9 +43,9 @@ function run_jmeter {
 
 
 
-for i in 100M 200M 400M 700M 3000M
+#for i in 100M 200M 400M 700M 3000M
 # for i in 100M 400M 3000M
-#for i in 3000M
+for i in 5000M
 do
    echo "About to run ${i}"
    set_io_limit ${i}
