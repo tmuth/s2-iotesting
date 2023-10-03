676111b39809128333afe7dfad50f7feab1d786d
tmuth <tmuth@splunk.com>
2023-10-03 22:14:02 +0000

    Updates

--- iostats.sh
+++ iostats.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+DEVICE=${1:-md0}
+echo "iostats for device: ${DEVICE}"
+
 red='\033[01;31m'
 none='\033[0m' 
- # gawk '{if($1 ~ /xvda/) {printf("%s","foo")} else printf("%s","bar")} {if((NR==3 && $1 ~ /Device/) || $1 ~ /xvda/ ) printf("%-25s %-10s %-10s %-8s %-9s %-6s\n",strftime("[%Y-%m-%d %H:%M:%S]"),$8,$9,$12,$13,$23)}'
- #
- iostat xvda -xd -m 1 | \
- gawk -v r="$red" -v n="$none" '  { if ( $1 ~ /xvda/ || ( NR==3 && $1 ~ /Device/ ) ) { sub("Device"," ",$1) ;  sub("xvda",strftime("[%Y-%m-%d %H:%M:%S]"),$1) ; \
-    printf("%-25s %10s " r "%10s" n "%12s %9s %6s\n",$1,$8,$9,$12,$13,$23)}}'
+
+# Use the following block to find the column numbers:
+# iostat ${DEVICE} -xd -m 1 1 | \
+# awk 'BEGIN{ FS=" " }
+#      { if ( $1 ~ /Device/ ) {for(fn=1;fn<=NF;fn++) {print fn" = "$fn;}; exit;} }'
+
+# writes: $1,$8,$9,$12,$13,$23
+# reads: $1,$2,$3,$6,$7,$23
+iostat ${DEVICE} -xd -m 1 | \
+ gawk -v r="$red" -v n="$none" -v dev="${DEVICE}" '  { if ( ($1 ~ dev && NR >= 5) || ( NR==3 && $1 ~ /Device/ ) ) { sub("Device"," ",$1) ;  sub(dev,strftime("[%Y-%m-%d %H:%M:%S]"),$1) ; \
+    printf("%-25s %10s " r "%10s" n "%12s %9s %6s\n",$1,$2,$3,$6,$7,$23)}}'
+
---
+++ run-cached-tests.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+INDEX_NAME=git
+HOME_PATH_PREFIX=/mnt/nvme/splunk
+EARLIEST="01/01/2023:06:00:00"
+LATEST="01/02/2023:22:00:00"
+NAME_PREFIX="test_a"
+
+IO_READ_LIMIT_BYTES=""
+IO_READ_LIMIT_MB=""
+
+
+function set_io_limit {
+    systemctl set-property Splunkd.service IOWriteBandwidthMax="/mnt/nvme $1"
+    systemctl show --property IOWriteBandwidthMax Splunkd.service
+
+    systemctl set-property Splunkd.service IOReadBandwidthMax="/mnt/nvme $1"
+    systemctl show --property IOReadBandwidthMax Splunkd.service
+
+    IO_READ_LIMIT_BYTES=$(systemctl show --property IOReadBandwidthMax Splunkd.service | awk -F ' ' '{print $2}')
+    IO_READ_LIMIT_MB=$(expr $IO_READ_LIMIT_BYTES / 1000 / 1000)
+    echo "IO_READ_LIMIT_MB: ${IO_READ_LIMIT_MB}"
+}
+
+function drop_system_cache {
+    echo 3 > /proc/sys/vm/drop_caches
+}
+
+function evict_cache {
+    echo "evict cache for ${INDEX_NAME} "
+    splunk _internal call /services/admin/cacheman/_evict -post:mb 1000000000 -post:path ${HOME_PATH_PREFIX}/${INDEX_NAME} -method POST -auth 'admin:welcome1'
+    du -sh /mnt/nvme/splunk/* | grep ${INDEX_NAME}
+}
+
+function run_jmeter {
+    # $1 : IO_LIMIT
+    # $2 : CACHED
+    # $3 : RECREATE_LOOKUP
+    echo "Runnig run_jemeter"
+    echo "${1} ${2} ${3}"
+    jmeter -n -t jmeter-s2-searches.jmx -JINDEX_NAME=${INDEX_NAME} -JOUTPUT_LOOKUP_FILE=${NAME_PREFIX}${1}.csv -JIO_LIMIT=${1} -JCACHED=${2} -JRECREATE_LOOKUP=${3} -JEARLIEST=${EARLIEST} -JLATEST=${LATEST} -l jmeter-logs/${NAME_PREFIX}${1}.jtl
+}
+
+
+
+#for i in 100M 200M 400M 700M 3000M
+# for i in 100M 400M 3000M
+for i in 5000M
+do
+   echo "About to run ${i}"
+   #set_io_limit ${i}
+   #evict_cache
+   #drop_system_cache
+   #run_jmeter "${i}" "uncached" "true"
+   run_jmeter "${i}" "cached" "false"
+done
